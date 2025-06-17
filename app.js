ee0832022e7563a3c644fb6dfba8fa8728188a77
Leya <rainandsoup@gmail.com>
2025-06-17 20:37:05 +0300

    Новый package-lock.json после обновления node

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.static(path.join(process.cwd(), "dist")));
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
